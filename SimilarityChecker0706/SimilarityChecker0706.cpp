6dabb8f8d7b93bdba3d7781b10b3962ffe5b6450
GyudongShim <gdshim@gmail.com>
2023-07-06 11:36:20 +0900

    [Feature] Partial length point

--- SimilarityChecker0706/SimilarityChecker0706.cpp
+++ SimilarityChecker0706/SimilarityChecker0706.cpp
@@ -1,9 +1,34 @@
 ﻿#pragma once
 
+#include <stdexcept>
+using namespace std;
 
 class SimilarityChecker
 {
 public:
+	explicit SimilarityChecker(const string& str)
+	{
+		originalString = str;
+	}
 
-	
+	int GetLegthPoints(const string& string)
+	{
+		if (string.length() == originalString.length())
+			return MAX_LENGTH_POINT;
+
+		// Too short to be checked
+		if (string.length() * 2 <= originalString.length())
+			return 0;
+
+		// Too long to be checked
+		if (string.length()  >= originalString.length() * 2)
+			return 0;
+
+		return 0;
+	}
+
+
+private:
+	string originalString;
+	static constexpr int MAX_LENGTH_POINT = 60;
 };
--- SimilarityCheckerTest/SimilarityCheckerTest.cpp
+++ SimilarityCheckerTest/SimilarityCheckerTest.cpp
@@ -1,6 +1,56 @@
 #include "pch.h"
+#include "../SimilarityChecker0706/SimilarityChecker0706.cpp"
 
-TEST(SimilarityChecker, GetTotalLength) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+
+using namespace std;
+
+
+class SimilarityCheckerTestFixture : public testing::Test
+{
+public:
+	SimilarityChecker* m_pChecker = nullptr;
+
+	void SetUp() override
+	{
+		m_pChecker = new SimilarityChecker(string("ABCD"));
+	}
+
+	void TearDown() override
+	{
+		delete m_pChecker;
+	}
+
+	void AssertIllegalArgument(string inputString)
+	{
+		try
+		{
+			m_pChecker->GetLegthPoints(inputString);
+			FAIL();
+		} catch (exception e)
+		{
+			// PASS
+		}
+	}
+};
+
+
+// To be used for 2nd
+TEST_F(SimilarityCheckerTestFixture, AssertIllegalInputs) {
+
+	// Long length
+	// AssertIllegalArgument("DDDDDDDD");
+}
+
+TEST_F(SimilarityCheckerTestFixture, GetTotalLengthWithSameLength) {
+	int lengthPoint = m_pChecker->GetLegthPoints("ABCD");
+	EXPECT_EQ(60, lengthPoint);
+}
+
+
+TEST_F(SimilarityCheckerTestFixture, GetTotalLegnthMoreThanDouble) {
+	// Half of given length
+	EXPECT_EQ(0, m_pChecker->GetLegthPoints("AB"));
+
+	// doubles of given length
+	EXPECT_EQ(0, m_pChecker->GetLegthPoints("ABCDABCD"));
 }
